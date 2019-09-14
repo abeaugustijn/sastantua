14b49bdd08a6ba0d573516cce74c9c320490a598
Abe Augustijn <aaugusti@f0r1s8.codam.nl>
2019-09-14 11:57:00 +0200

    Added calc_bottom_row_len function

---
+++ sastantua.c
@@ -0,0 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   sastantua.c                                        :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: aaugusti <marvin@codam.nl>                   +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2019/09/14 10:55:36 by aaugusti      #+#    #+#                 */
+/*   Updated: 2019/09/14 11:55:09 by aaugusti      ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+int		calc_bottom_row_len(int size)
+{
+	int res;
+	int n;
+	int r;
+	
+	res = 0;
+	n = 1;
+	r = 0;
+	while (n <= size)
+	{
+		res += 3;
+		if(n > 1)
+			res += 3;
+		r = 0;
+		while (r < n+1)
+		{
+			res += 2;
+			r++;
+		}
+		n++;
+	}
+	return (res);
+}
+
+void	sastantua(int size)
+{
+	printf("%d\n", calc_bottom_row_len(size));
+}
