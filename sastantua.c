7beb856530ffb6dd17a115e94ae2a1eac07a41b7
Abe Augustijn <aaugusti@f0r1s8.codam.nl>
2019-09-14 12:05:04 +0200

    Pleased norminette

--- sastantua.c
+++ sastantua.c
@@ -6,7 +6,7 @@
 /*   By: aaugusti <marvin@codam.nl>                   +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2019/09/14 10:55:36 by aaugusti      #+#    #+#                 */
-/*   Updated: 2019/09/14 11:55:09 by aaugusti      ########   odam.nl         */
+/*   Updated: 2019/09/14 12:04:50 by aaugusti      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,17 +17,17 @@ int		calc_bottom_row_len(int size)
 	int res;
 	int n;
 	int r;
-	
+
 	res = 0;
 	n = 1;
 	r = 0;
 	while (n <= size)
 	{
 		res += 3;
-		if(n > 1)
+		if (n > 1)
 			res += 3;
 		r = 0;
-		while (r < n+1)
+		while (r < n + 1)
 		{
 			res += 2;
 			r++;
